b8e78238199e1773d2456693d40a1192621586af
brenosekiro <b88660501@gmail.com>
2020-05-07 13:58:12 -0300

    Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import './Header.css'
-import Image from '../assets/imgs/logo.png'
 
 export default props => (
     <header>
         <div className="logo">
-            <h3>SpaceCoding</h3>
-            <div className='logo-img'></div>
+            <h3 className="logo-text">SpaceCoding</h3>
+             <div className="logo-img"></div>
         </div>
     </header>
 )
