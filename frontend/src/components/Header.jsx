1a2491ceadd70144036f3adab30f5e78ea98f763
brenosekiro <b88660501@gmail.com>
2020-05-06 23:52:20 -0300

    first commit

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+import Image from '../assets/imgs/logo.png'
+
+export default props => (
+    <header>
+        <div className="logo">
+            <h3>SpaceCoding</h3>
+            <div className='logo-img'></div>
+        </div>
+    </header>
+)
