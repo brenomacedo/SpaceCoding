9a350c2cb165740efb3fe66513c8170d22decae9
brenosekiro <b88660501@gmail.com>
2020-05-08 23:25:36 -0300

    addPhoto

--- frontend/src/components/AddPost.jsx
+++ frontend/src/components/AddPost.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { FaCamera } from 'react-icons/fa'
 import './AddPost.css'
 
 export default props => {
@@ -8,7 +9,10 @@ export default props => {
     return (
         <div className="add-post-wrapper">
             <div className="add-post">
-                <h3>Add a doubt or a publicity!</h3>
+                <div className="add-post-header">
+                    <h3>Add a doubt or a publicity!</h3>
+                    <FaCamera style={{ marginRight: 5, cursor: 'pointer' }} size={20} color='white' />
+                </div>
                 <textarea value={description} onChange={e => setDescription(e.target.value)}></textarea>
             </div>
         </div>
