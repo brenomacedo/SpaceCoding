6131996e0a80294c69082cd5f275ff1c6dc2f90d
brenosekiro <b88660501@gmail.com>
2020-05-11 19:26:27 -0300

    securityPassword

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,20 +1,33 @@
 const knex = require('../../config/db')
+const bcrypt = require('bcryptjs')
 
-exports.createUser = (req, res) => {
+exports.createUser = async (req, res) => {
+
+    const password = await bcrypt.hash(req.body.password, 10)
+
     knex('users').insert({
         name: req.body.name,
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
+        password: password,
         image: req.body.image
     }).then(resp => res.send('SUCESSO'))
     .catch(err => res.send(err))
 }
 
-exports.getUser = (req, res) => {
-    knex('users').where({ username: req.query.username, password: req.query.password })
-    .orWhere({ email: req.query.email, password: req.query.password })
+exports.getUser = async (req, res) => {
+    const User = await knex('users').where({ username: req.query.username})
+    .orWhere({ email: req.query.email })
     .first()
-    .then(resp => res.json(resp))
-    .catch(err => res.send(err))
+
+    if(!User) {
+        return res.send('USUARIO OU SENHA INCORRETOS')
+    }
+
+    if(!await bcrypt.compare(req.query.password, User.password)) {
+        return res.send('USUARIO OU SENHA INCORRETOS')
+    }
+
+    return res.json(User)
+
 }
